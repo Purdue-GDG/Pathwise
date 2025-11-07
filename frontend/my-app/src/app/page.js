eafe05ef51e236c8d9e050f0485f6c76a0e6a668
Aastha Patel <aasthajpatel@gmail.com>
2025-11-07 13:47:54 -0500

    add select components
    add select inputs

--- frontend/my-app/src/app/page.js
+++ frontend/my-app/src/app/page.js
@@ -1,95 +1,45 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Form from "next/form";
+
+async function createPost(formData) {
+  'use server';
+  const title = formData.get('title');
+  console.log(title);
+}
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Pathwise Project start!!
-          </li>
-          <li>Let's do the deliverables...</li>
-        </ol>
+    <div className={styles.formContainer}>
+      <div>Select your major</div>
+      <form action={createPost} className={styles.formField}>
+        <select name="title" className={styles.inputField}>
+          <option value="AI">AI</option>
+          <option value="CS">CS</option>
+          <option value="DS">DS</option>
+        </select>
+        <button type="submit" className={styles.submitButton}>Submit</button>
+      </form>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <form action={createPost}>
+        <div>What primarily describes your academic interests?</div>
+        <select name="title" className={styles.inputField}>
+          <option value="Biotech">Biotech</option>
+          <option value="Finance">Finance</option>
+          <option value="Psychology">Psychology</option>
+        </select>
+        <button type="submit" className={styles.submitButton}>Submit</button>
+      </form>
+
+      <form action={createPost}>
+        <div>Type of Courses</div>
+        <select name="title" className={styles.inputField}>
+          <option value="Graduation Plan">Graduation Plan</option>
+          <option value="Gen Eds">Gen Eds</option>
+          <option value="Electives">Electives</option>
+        </select>
+        <button type="submit" className={styles.submitButton}>Submit</button>
+      </form>
     </div>
   );
 }
